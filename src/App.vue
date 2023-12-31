d2bed0455905ccc6383fa790b8c4a1d4a57736c9
Danish Mehmood <danishmehmood981@gmail.com>
2024-01-01 00:00:01 +0500

    cards styled with bs4

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,25 @@
 
 <script setup>
   import { ref } from 'vue';
-
+  import quizzes from './data/data.json';
 </script>
 
 <template>
   <main>
-
     <div class="container mt-3">
       <header class="d-flex justify-content-between align-items-center">
         <h1>Quizzes</h1>
+        <input type="text" class="form-control ml-4 w-25" placeholder="search ...">
       </header>
-      <div class="row">
+      <div class="row mt-4">
 
-        <div class="card col-md-3 m-2 p-0"  >
-          <div class="card-body">
-            <h5 class="card-title">Quiz title</h5>
-            <p class="card-text">quiz text and notes</p>
-            <p class="card-footer text-muted">quiz date</p>
+        <div class="card col-md-3 m-2 p-0 pb-2 text-dark border-0" style="height: 15rem;" v-for="quiz in quizzes"  :key="quiz.id" >
+          <img :src="quiz.img" alt="the quiz image" style=" object-fit: cover;">
+          <div class="card-body p-0 ml-3" >
+            <p class="card-text font-weight-bold mt-2 mb-1">{{ quiz.name }}</p>
+            <p class="card-text">{{ quiz.questions.length }} questions</p>
           </div>
         </div>
-        
-        <div class="card col-md-3 m-2 p-0"  >
-          <div class="card-body">
-            <h5 class="card-title">Quiz title</h5>
-            <p class="card-text">quiz text and notes</p>
-            <p class="card-footer text-muted">quiz date</p>
-          </div>
-        </div>
-
-        <div class="card col-md-3 m-2 p-0"  >
-          <div class="card-body">
-            <h5 class="card-title">Quiz title</h5>
-            <p class="card-text">quiz text and notes</p>
-            <p class="card-footer text-muted">quiz date</p>
-          </div>
-        </div>
-    
       </div>
     </div>
   </main>
@@ -55,8 +38,17 @@
   border-radius: 5px;
 }
 
+.card img {
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+  width: 100%;
+  height: 200px;
+  object-fit: cover;
+}
+
 .card:hover {
   box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+  cursor: pointer;
 }
 
 .card-title {
