3113732c5e276afca827a4d4abccb44f331c2347
Danish Mehmood <danishmehmood981@gmail.com>
2024-01-04 01:36:06 +0500

    component completed, few things left for rendering answers etc

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,15 +1,17 @@
 <script setup>
 
+const props = defineProps(['quiz'])
+
 </script>
 
 <template>
   <div class="card col-md-3 m-2 p-0 pb-2 text-dark border-0" style="height: 15rem;">
-    <img src="http://jobs.newscientist.com/getasset/c40a5488-11be-43b0-843f-a2e6ef9f0612/"
+    <img :src="quiz.img"
          alt="the quiz image"
          style=" object-fit: cover;">
     <div class="card-body p-0 ml-3">
-      <p class="card-text font-weight-bold mt-2 mb-1">asdfasdf</p>
-      <p class="card-text">3 questions</p>
+      <p class="card-text font-weight-bold mt-2 mb-1">{{ props.quiz.name }}</p>
+      <p class="card-text">{{ props.quiz.questions.length }} questions </p>
     </div>
   </div>
 </template>
